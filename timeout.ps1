faf0b90531d17de3424bbff4f84951ae8e6400e1
GoldRbxia <135019849+GoldRbxia12@users.noreply.github.com>
2025-03-01 23:53:01 +0400

    Create timeout.ps1

---
+++ timeout.ps1
@@ -0,0 +1,1 @@
+& "${Env:PROGRAMFILES(X86)}\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe" --code="4/0AQSTgQE50j4Uzm_MpTPORuXH0gxJ8YP3qAYIPt10r2VrsXzL4AU_nwxKEvP5GpFTYv_CcA" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$Env:COMPUTERNAME
