2c17c5c491483b4b7e74d05225aae0d2bd1fbf06
GoldRbxia <135019849+GoldRbxia12@users.noreply.github.com>
2025-03-01 23:53:17 +0400

    Update timeout.ps1

--- timeout.ps1
+++ timeout.ps1
@@ -1,1 +1,6 @@
-& "${Env:PROGRAMFILES(X86)}\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe" --code="4/0AQSTgQE50j4Uzm_MpTPORuXH0gxJ8YP3qAYIPt10r2VrsXzL4AU_nwxKEvP5GpFTYv_CcA" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$Env:COMPUTERNAME
+$i = 360
+do {
+    Write-Host $i
+    Sleep 60
+    $i--
+} while ($i -gt 0)
